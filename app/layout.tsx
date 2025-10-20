1295f71a7e0be42b47a50a4551a5b57a068edab4
Adil <adilsaaly@gmail.com>
2025-10-20 21:36:51 +0530

    pwa

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ServiceWorkerRegister from "@/components/pwa";
 
 // Load fonts
 const geistSans = Geist({
@@ -47,6 +48,7 @@ export default function RootLayout({
 <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ServiceWorkerRegister />
         {children}
       </body>
     </html>
